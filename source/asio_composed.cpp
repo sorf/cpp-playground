a49a90441c1b70129e6b6bc8e898338ed8290b4b
Sorin Fetche <sorin.fetche@gmail.com>
2019-01-11 23:15:58 -0800

    asio_composed implemented in terms of shared_async_state.

--- source/asio_composed.cpp
+++ source/asio_composed.cpp
@@ -2,6 +2,7 @@
 //#define BOOST_ASIO_ENABLE_HANDLER_TRACKING
 
 #include "bind_allocator.hpp"
+#include "on_scope_exit.hpp"
 #include "shared_async_state.hpp"
 #include "stack_handler_allocator.hpp"
 
@@ -15,7 +16,6 @@
 #include <boost/format.hpp>
 #include <boost/predef.h>
 #include <boost/range/irange.hpp>
-#include <boost/scope_exit.hpp>
 #include <cassert>
 #include <chrono>
 #include <iostream>
@@ -42,16 +42,9 @@ auto async_many_timers(asio::io_context &io_context, bool &user_resource,
                        std::chrono::steady_clock::duration run_duration, CompletionToken &&token) ->
     typename asio::async_result<std::decay_t<CompletionToken>, void(error_code)>::return_type {
 
-    using shared_state_type =
-        async_utils::shared_async_state<void(error_code), CompletionToken, asio::io_context::executor_type>;
-#if 0
-    struct internal_op {
-        using shared_state_type = async_utils::shared_async_state<void(error_code), CompletionToken,
-                                                                  asio::io_context::executor_type, internal_op>;
-
-        internal_op(asio::io_context &io_context, bool &user_resource)
-            : user_resource{user_resource}, run_timer{io_context}, is_open{false}, executing{false}, pending_async{} {
-
+    struct state_data {
+        state_data(asio::io_context &io_context, bool &user_resource)
+            : user_resource{user_resource}, run_timer{io_context}, executing{false}, is_open{false} {
             int timer_count = 25;
             internal_timers.reserve(timer_count);
             for (int timer_index = 0; timer_index < timer_count; ++timer_index) {
@@ -59,105 +52,109 @@ auto async_many_timers(asio::io_context &io_context, bool &user_resource,
             }
         }
 
-        void start_many_waits(shared_state_type &&state, std::chrono::steady_clock::duration run_duration) {
-            assert(user_resource);
-            assert(!executing);
-            executing = true;
-            BOOST_SCOPE_EXIT_ALL(&) { executing = false; };
-
-            // Setting the total run duration.
-            run_timer.expires_after(run_duration);
-            run_timer.async_wait(state->wrap()([this, state](error_code ec) mutable {
-                assert(user_resource);
-                assert(!executing);
-                executing = true;
-                BOOST_SCOPE_EXIT_ALL(&) { executing = false; };
-                assert(pending_async);
-                pending_async--;
-
-                close(ec);
-                invoke_if_last_handler(state);
-            }));
-            ++pending_async;
-
-            // Starting the internal timers in parallel.
-            for (auto timer_index : boost::irange<std::size_t>(0, internal_timers.size())) {
-                start_one_wait(shared_state_type{state}, timer_index,
-                               std::chrono::milliseconds(20 * (timer_index + 1)));
-            }
-            std::cout << "timers started" << std::endl;
-            is_open = true;
-        }
-
-        void close(error_code ec) {
-            if (!is_open) {
-                return;
-            }
-            user_completion_error = ec;
-            is_open = false;
-            run_timer.cancel();
-            for (auto &timer : internal_timers) {
-                timer.cancel();
-            }
-            std::cout << "timers cancelled" << std::endl;
-        }
-
-        void invoke_if_last_handler(shared_state_type &state) {
-            if (0 == pending_async) {
-                auto ec = user_completion_error; // anything used in invoke needs to be on the stack
-                state->invoke(ec);
-            }
-        }
-
-        void start_one_wait(shared_state_type &&state, std::size_t timer_index,
-                            std::chrono::steady_clock::duration one_wait) {
-            auto &timer = internal_timers[timer_index];
-            timer.expires_after(one_wait);
-            timer.async_wait(state->wrap()([=, state = std::move(state)](error_code ec) mutable {
-                assert(user_resource);
-                assert(!executing);
-                executing = true;
-                BOOST_SCOPE_EXIT_ALL(&) { executing = false; };
-                assert(pending_async);
-                pending_async--;
-
-                if (is_open && !ec) {
-                    start_one_wait(std::move(state), timer_index, one_wait);
-                    return;
-                }
-
-                // Operation complete here.
-                if (!is_open) {
-                    std::cout << boost::format("timer[%d]: wait closed") % timer_index << std::endl;
-                } else {
-                    std::cout << boost::format("timer[%d]: wait error: %s:%s") % timer_index % ec % ec.message()
-                              << std::endl;
-                    close(ec);
-                }
-                invoke_if_last_handler(state);
-            }));
-            ++pending_async;
-        }
+        state_data(state_data const &) = delete;
+        state_data(state_data &&) = delete;
+        state_data &operator=(state_data const &) = delete;
+        state_data &operator=(state_data &&other) = delete;
+        ~state_data() { assert(!executing); }
 
         bool &user_resource;
         error_code user_completion_error;
         asio::steady_timer run_timer;
         std::vector<asio::steady_timer> internal_timers;
-        bool is_open;
+        // Atomic flag to detect if internal completion handlers run in parallel.
         std::atomic_bool executing;
-        std::size_t pending_async;
+        // If this is not opened anymore, close() has no effect.
+        bool is_open;
     };
-#endif
-    assert(user_resource);
-    typename shared_state_type::completion_type completion(token);
-    [[maybe_unused]] shared_state_type state{std::move(completion.completion_handler), io_context.get_executor()};
-    (void)run_duration;
-#if 0
-    auto state = async_utils::make_shared_async_state<typename internal_op::shared_state_type>(
-        std::move(completion.completion_handler), io_context.get_executor(), io_context, user_resource);
-    auto *op = state->get_data();
-    op->start_many_waits(std::move(state), run_duration);
-#endif
+
+    using base_type =
+        async_utils::shared_async_state<void(error_code), CompletionToken, asio::io_context::executor_type, state_data>;
+    struct internal_op : base_type {
+        state_data &data;
+
+        internal_op(asio::io_context &io_context, bool &user_resource, std::chrono::steady_clock::duration run_duration,
+                    typename base_type::completion_handler_type &&completion_handler)
+            : base_type{io_context.get_executor(), std::move(completion_handler), io_context, user_resource},
+              data{base_type::get_data()} {
+
+            assert(data.user_resource);
+            assert(!data.executing);
+            data.executing = true;
+            ON_SCOPE_EXIT([&]{ data.executing = false; }); // NOTE: This works because we copy this in the lambda below
+
+            // Setting the total run duration.
+            data.run_timer.expires_after(run_duration);
+            data.run_timer.async_wait(this->wrap([*this](error_code ec) mutable {
+                {
+                    assert(data.user_resource);
+                    assert(!data.executing);
+                    data.executing = true;
+                    ON_SCOPE_EXIT([&]{ data.executing = false; });
+                    close(ec);
+                }
+                invoke_if_last_handler(); // Note: The executing flag accessed on-scope-exit has to be reset
+                                          // before we deallocate the state data and call the final handler
+            }));
+
+            // Starting the internal timers in parallel.
+            for (auto timer_index : boost::irange<std::size_t>(0, data.internal_timers.size())) {
+                start_one_wait(timer_index, std::chrono::milliseconds(20 * (timer_index + 1)));
+            }
+            std::cout << "timers started" << std::endl;
+            data.is_open = true;
+        }
+
+        void start_one_wait(std::size_t timer_index, std::chrono::steady_clock::duration one_wait) {
+            auto &timer = data.internal_timers[timer_index];
+            timer.expires_after(one_wait);
+            timer.async_wait(this->wrap([=, *this](error_code ec) mutable {
+                {
+                    assert(data.user_resource);
+                    assert(!data.executing);
+                    data.executing = true;
+                    ON_SCOPE_EXIT([&]{ data.executing = false; });
+
+                    if (data.is_open && !ec) {
+                        return start_one_wait(timer_index, one_wait);
+                    }
+
+                    // Operation complete here.
+                    if (!data.is_open) {
+                        std::cout << boost::format("timer[%d]: wait closed") % timer_index << std::endl;
+                    } else {
+                        std::cout << boost::format("timer[%d]: wait error: %s:%s") % timer_index % ec % ec.message()
+                                << std::endl;
+                        close(ec);
+                    }
+                }
+                invoke_if_last_handler(); // Note: Same as above, invoke() after on-scope-exit has run
+            }));
+        }
+
+        void close(error_code ec) {
+            if (!data.is_open) {
+                return;
+            }
+            data.user_completion_error = ec;
+            data.is_open = false;
+            data.run_timer.cancel();
+            for (auto &timer : data.internal_timers) {
+                timer.cancel();
+            }
+            std::cout << "timers cancelled" << std::endl;
+        }
+
+        void invoke_if_last_handler() {
+            if (this->unique()) {
+                auto ec = data.user_completion_error; // anything used in invoke needs to not be in state data anymore
+                this->invoke(ec);
+            }
+        }
+    };
+
+    typename internal_op::completion_type completion(token);
+    internal_op op{io_context, user_resource, run_duration, std::move(completion.completion_handler)};
     return completion.result.get();
 }
 
