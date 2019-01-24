9ae74a6cc9cea595c81c8e2722cd046b8bfbdd69
Sorin Fetche <sorin.fetche@gmail.com>
2019-01-23 18:32:37 -0800

    leaf-test with async operation

--- source/leaf_test.cpp
+++ source/leaf_test.cpp
@@ -1,9 +1,14 @@
 #include <algorithm>
+#include <boost/asio/associated_executor.hpp>
+#include <boost/asio/bind_executor.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/post.hpp>
 #include <boost/leaf/all.hpp>
 #include <iostream>
 #include <string>
 #include <system_error>
 
+namespace asio = boost::asio;
 namespace leaf = boost::leaf;
 
 struct e_failure_info {
@@ -59,40 +64,65 @@ leaf::result<unsigned> f2(unsigned behavior) {
     return behavior;
 }
 
-int main() {
+struct async_operation {
+  private:
+    template <typename Handler> static decltype(auto) get_executor(asio::io_context &io_context, Handler &h) {
+        return asio::get_associated_executor(h, io_context.get_executor());
+    }
 
+  public:
+    template <typename Handler> static void start(asio::io_context &io_context, unsigned behavior, Handler &&h) {
+        asio::post(io_context,
+                   asio::bind_executor(get_executor(io_context, h), [behavior, h = std::forward<Handler>(h)]() mutable {
+                       execute(behavior, std::move(h));
+                   }));
+    }
+
+  private:
+    template <typename Handler> static void execute(unsigned behavior, Handler &&h) {
+        auto result = leaf::capture_result<std::exception, e_failure_info>(
+            [behavior]() -> leaf::result<int> { return f2(behavior); });
+        h(result());
+    }
+};
+
+int main() {
     for (unsigned i = 0; i < 10; ++i) {
+        asio::io_context io_context;
         std::cout << "\nf2(" << i << ")" << std::endl;
-        leaf::result<int> result = leaf::try_(
-            [i]() -> leaf::result<int> {
-                LEAF_CHECK(f2(i));
-                std::cout << "Success" << std::endl;
-                return 0;
-            },
-            [](leaf::catch_<std::runtime_error> e, e_failure_info const &info) -> leaf::result<int> {
-                std::cout << "Error: runtime_error: " << e.value.what() << ", info: " << info.value << std::endl;
-                return -1;
-            },
-            [](leaf::catch_<std::runtime_error> e) -> leaf::result<int> {
-                std::cout << "Error: runtime_error: " << e.value.what() << std::endl;
-                return -2;
-            },
-            [](custom_error_code ec, e_failure_info const &info) -> leaf::result<int> {
-                std::cout << "Error: code: " << ec << ", info: " << info.value << std::endl;
-                return -3;
-            },
-            [](custom_error_code ec) -> leaf::result<int> {
-                std::cout << "Error: code: " << ec << std::endl;
-                return -4;
-            },
-            [](leaf::error_info const &e /*, leaf::verbose_diagnostic_info const *v*/) -> leaf::result<int> {
-                std::cout << "Error: unmatched error, what: " << (e.has_exception() ? e.exception()->what() : "<NA>")
-                          << std::endl;
-                /*if (v) {
-                    std::cout << "Error: verbose-info: " << *v << std::endl;
-                }*/
-                return -5;
-            });
+        async_operation::start(io_context, i, [](leaf::result<int> result) {
+            leaf::try_(
+                [&result]() -> leaf::result<void> {
+                    LEAF_CHECK(result);
+                    std::cout << "Success" << std::endl;
+                    return {};
+                },
+                [](leaf::catch_<std::runtime_error> e, e_failure_info const &info) -> leaf::result<void> {
+                    std::cout << "Error: runtime_error: " << e.value.what() << ", info: " << info.value << std::endl;
+                    return {};
+                },
+                [](leaf::catch_<std::runtime_error> e) -> leaf::result<void> {
+                    std::cout << "Error: runtime_error: " << e.value.what() << std::endl;
+                    return {};
+                },
+                [](custom_error_code ec, e_failure_info const &info) -> leaf::result<void> {
+                    std::cout << "Error: code: " << ec << ", info: " << info.value << std::endl;
+                    return {};
+                },
+                [](custom_error_code ec) -> leaf::result<void> {
+                    std::cout << "Error: code: " << ec << std::endl;
+                    return {};
+                },
+                [](leaf::error_info const &e /*, leaf::verbose_diagnostic_info const *v*/) -> leaf::result<void> {
+                    std::cout << "Error: unmatched error, what: "
+                              << (e.has_exception() ? e.exception()->what() : "<NA>") << std::endl;
+                    /*if (v) {
+                        std::cout << "Error: verbose-info: " << *v << std::endl;
+                    }*/
+                    return {};
+                });
+        });
+        io_context.run();
     }
     return 0;
 }
