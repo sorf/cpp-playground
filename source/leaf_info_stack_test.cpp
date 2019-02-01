e00db923f5886d59dace05bfaf242fd75c81905d
Sorin Fetche <sorin.fetche@gmail.com>
2019-01-31 17:31:00 -0800

    leaf_info_stack_test.cpp: Upgrade to latest LEAF. Try a `defer_accumulate` utility that creates functions that will call leaf::accumulate.

--- source/leaf_info_stack_test.cpp
+++ source/leaf_info_stack_test.cpp
@@ -8,41 +8,61 @@ struct e_stack {
     std::nullptr_t value;
 };
 
-template <typename ErrorHandler, typename F> decltype(auto) initiate_operation(F &&f) {
-    [[maybe_unused]] auto a_initiate = leaf::accumulate([](e_stack &) { std::cout << "stack: initiate\n"; });
+template <typename F> decltype(auto) defer_accumulate(F &&f) {
+    return [f = std::forward<F>(f)] { return leaf::accumulate(std::move(f)); };
+}
 
-    return [f = std::move(f), a_initiate = std::move(a_initiate)] {
-        [[maybe_unused]] auto a_c1 = leaf::accumulate([](e_stack &) { std::cout << "stack: continuation1\n"; });
-        auto call = leaf::capture_in_result<ErrorHandler>([]() -> leaf::result<int> {
-            return leaf::exception_to_result([]() -> leaf::result<int> {
-                [[maybe_unused]] auto a_c2 = leaf::accumulate([](e_stack &) { std::cout << "stack: continuation2\n"; });
-                throw std::runtime_error("X");
-            });
-        });
-        f(call());
+template <typename LeafAccumulator, typename F> decltype(auto) defer_accumulate(LeafAccumulator &&acc, F &&f) {
+    return [prev_acc = std::forward<LeafAccumulator>(acc), f = std::forward<F>(f)] {
+        return std::make_pair(leaf::accumulate(std::move(f)), prev_acc());
     };
 }
 
-int main(void) {
-    auto error_handler = [](leaf::error const &error) {
-        return leaf::handle_error(error, [](leaf::verbose_diagnostic_info const &diag, e_stack const *e) {
-            std::cout << "Error: "
-                      << "any_stack: " << (e ? "YES" : "NO") << ", diagnostic:" << diag << std::endl;
+template <typename ErrorHandler, typename F> decltype(auto) initiate_operation(F &&f) {
+    auto acc = defer_accumulate([](e_stack &) { std::cout << "stack: initiate\n"; });
+
+    return [f = std::forward<F>(f), acc_prev = std::move(acc)] {
+        auto acc = defer_accumulate(std::move(acc_prev), [](e_stack &) { std::cout << "stack: continuation1\n"; });
+
+        auto r = leaf::capture_in_result<ErrorHandler>([acc_prev = std::move(acc)]() -> leaf::result<int> {
+            auto acc = defer_accumulate(std::move(acc_prev), [](e_stack &) { std::cout << "stack: continuation2\n"; });
+
+            return leaf::exception_to_result([acc_prev = std::move(acc)]() -> leaf::result<int> {
+                auto acc =
+                    defer_accumulate(std::move(acc_prev), [](e_stack &) { std::cout << "stack: continuation3\n"; });
+
+                [[maybe_unused]] auto acc_now = acc();
+                throw std::runtime_error("X");
+            });
         });
+        f(r);
     };
+}
 
-    auto continuation = initiate_operation<decltype(error_handler)>([&](leaf::result<int> result) {
-        leaf::bound_handle_all(
-            [&result]() -> leaf::result<void> {
-                LEAF_CHECK(result);
-                std::cout << "Success" << std::endl;
-                return {};
-            },
-            [&](leaf::error const &error) { return error_handler(error); });
-    });
+int main() {
+    try {
+        auto error_handler = [](leaf::error_in_remote_handle_all const &error) {
+            return leaf::handle_error(error, [](leaf::verbose_diagnostic_info const &diag, e_stack const *e) {
+                std::cout << "Error: "
+                          << "any_stack: " << (e != nullptr ? "YES" : "NO") << ", diagnostic:" << diag << std::endl;
+            });
+        };
 
-    auto f = std::async(std::move(continuation));
-    f.get();
+        auto continuation = initiate_operation<decltype(error_handler)>([&](leaf::result<int> result) {
+            leaf::remote_handle_all(
+                [&result]() -> leaf::result<void> {
+                    // NOLINTNEXTLINE
+                    LEAF_CHECK(result);
+                    std::cout << "Success" << std::endl;
+                    return {};
+                },
+                [&](leaf::error_in_remote_handle_all const &error) { return error_handler(error); });
+        });
 
+        auto f = std::async(std::move(continuation));
+        f.get();
+    } catch (std::exception const &e) {
+        std::cout << "Error: " << e.what() << "\n";
+    }
     return 0;
 }
